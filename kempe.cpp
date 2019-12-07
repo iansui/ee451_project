16e5f1c4ea45a4f0a2661db50e6fc265da34beba
Patric-Lee <lipengc@pku.edu.cn>
2019-12-06 20:13:10 -0800

    Add command line parameter to graph partition

--- kempe.cpp
+++ kempe.cpp
@@ -10,8 +10,8 @@ using std::endl;
 // Return a random number in [0, 1]
 float getRandomNumber() {
     //srand(time(NULL));
-    //return (float)rand() / RAND_MAX;
-    return 1;
+    return (float)rand() / RAND_MAX;
+    
 }
 
 int infect(std::vector<std::unordered_map<Node, float> >& graph, std::unordered_set<Node>& seed) {
